ca450cf599b09374081c1091c4a5e78734eece5f
surfer19 <marianmrva123@gmail.com>
2020-01-19 15:13:10 +0000

    Deploying to gh-pages from master 84538557407a57f636e11aeb8510bf454d08db2b

---
+++ precache-manifest.8872493322e3e2fa7c347bdefe2b2ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cd6d046c3e4415273ce42882562e11",
+    "url": "/green-roofs/index.html"
+  },
+  {
+    "revision": "a2d2690f7e44ec7a4fa4",
+    "url": "/green-roofs/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1b206137e92371b5cc1b",
+    "url": "/green-roofs/static/js/2.99352bd4.chunk.js"
+  },
+  {
+    "revision": "a2d2690f7e44ec7a4fa4",
+    "url": "/green-roofs/static/js/main.2ebe9c02.chunk.js"
+  },
+  {
+    "revision": "8ee317ea4e43ebdca64b",
+    "url": "/green-roofs/static/js/runtime-main.c4ba69b7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/green-roofs/static/media/logo.25bf045c.svg"
+  }
+]);
